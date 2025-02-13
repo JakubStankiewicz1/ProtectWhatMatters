f9f4dae85f284db4ba42a4873ae2d7e68f5a00f5
Jakub Stankiewicz <272264@student.pwr.edu.pl>
2025-02-13 09:03:41 +0100

    AboutUs component update

--- frontend/src/components/AboutUs/AboutUs.jsx
+++ frontend/src/components/AboutUs/AboutUs.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
+import React from "react";
 import "./aboutUs.css";
 
 const AboutUs = () => {
   return (
     <div className="aboutUs">
-
-        <div className="aboutUsContainer">
-
-            {/* Top Part */}
-            <div className="aboutUsContainerTopPart">
-                <div className="aboutUsContainerTopPartDotElement" />
-                <div className="aboutUsContainerTopPartText">ABOUT US</div>
-            </div>
-
-            {/* Bottom Part */}
-            <div className="aboutUsContainerBottomPart">
-                <div className="aboutUsContainerBottomPartText">
-                    OUR VISION IS TO PROTECT PEOPLE IN THE BEST POSSIBLE WAY AT A PRICE WHERE EVERYONE CAN PARTICIPATE
-                </div>
-            </div>
-
+      <div className="aboutUsContainer">
+        {/* Top Part */}
+        <div className="aboutUsContainerTopPart">
+          <div className="aboutUsContainerTopPartDotElement" />
+          <p className="aboutUsContainerTopPartText">ABOUT US</p>
         </div>
-        
-    </div>
-  )
-}
 
-export default AboutUs
+        {/* Bottom Part */}
+        <div className="aboutUsContainerBottomPart">
+          <p className="aboutUsContainerBottomPartText">
+            OUR VISION IS TO PROTECT PEOPLE IN THE BEST POSSIBLE WAY AT A PRICE
+            WHERE EVERYONE CAN PARTICIPATE
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
