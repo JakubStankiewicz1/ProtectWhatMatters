243269f16857e10f18277dd98b0261c8cba338fb
Jakub Stankiewicz <272264@student.pwr.edu.pl>
2025-02-13 13:54:09 +0100

    productCategories component update

--- frontend/src/components/LatestArrivals/LatestArrivals.jsx
+++ frontend/src/components/LatestArrivals/LatestArrivals.jsx
@@ -7,11 +7,14 @@ const LatestArrivals = () => {
   const [sortType, setSortType] = useState("default");
 
   // Sorting logic
-  const sortedProducts = [...productsData.products].sort((a, b) => {
-    if (sortType === "price") return a.price - b.price;
-    if (sortType === "date") return new Date(b.date_added) - new Date(a.date_added);
-    return a.id - b.id; // Default sorting by ID
-  }).slice(0, 7); // Limit to 7 products
+  const sortedProducts = [...productsData.products]
+    .sort((a, b) => {
+      if (sortType === "price") return a.price - b.price;
+      if (sortType === "date")
+        return new Date(b.date_added) - new Date(a.date_added);
+      return a.id - b.id; // Default sorting by ID
+    })
+    .slice(0, 6); // Limit to 7 products
 
   return (
     <div className="latestArrivals">
@@ -26,7 +29,9 @@ const LatestArrivals = () => {
               <option value="price">Price</option>
               <option value="date">Newest</option>
             </select>
-            <p className="productCount">Showing {sortedProducts.length} products</p>
+            <p className="productCount">
+              Showing {sortedProducts.length} products
+            </p>
           </div>
         </div>
 
@@ -50,8 +55,9 @@ const LatestArrivals = () => {
           </div>
           <div className="rightSection">
             <p className="rightSectionText">
-              Protection Group Danmark manufactures and sells high-quality bulletproof and stab-proof vests,
-              bulletproof helmets, hard armor plates, and other ballistic equipment.
+              Protection Group Danmark manufactures and sells high-quality
+              bulletproof and stab-proof vests, bulletproof helmets, hard armor
+              plates, and other ballistic equipment.
             </p>
           </div>
         </div>
